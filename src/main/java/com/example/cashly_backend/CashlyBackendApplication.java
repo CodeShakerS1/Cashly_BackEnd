80e2a519cf4deecb46f58cd6ba6af664dc1b2487
dressaDevv <dr3sa.dev@gmail.com>
2026-03-25 13:50:03 -0300

    feat: add rotas de usuario

--- src/main/java/com/example/cashly_backend/CashlyBackendApplication.java
+++ src/main/java/com/example/cashly_backend/CashlyBackendApplication.java
@@ -2,32 +2,9 @@ package com.example.cashly_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 
-@RestController
 @SpringBootApplication
-@RequestMapping("/contato")
 public class CashlyBackendApplication {
-    
-    @GetMapping
-    public String Get() {
-        return "GET funcionando!";
-    }
-
-    @PostMapping
-    public String Post() {
-        return "POST funcionando!";
-    }
-
-    @PutMapping
-    public String Put() {
-        return "PUT funcionando!";
-    }
-
-    @DeleteMapping
-    public String Delete() {
-        return "DELETE funcionando!";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(CashlyBackendApplication.class, args);
