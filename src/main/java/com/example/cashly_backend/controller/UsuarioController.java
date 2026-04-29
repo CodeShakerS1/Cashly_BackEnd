05c04e1dc261172cf8ca5341ef3376e6863d382f
joaovsilva18 <joaovactorsilva@gmail.com>
2026-04-29 12:37:32 -0300

    feat: partial login route

--- src/main/java/com/example/cashly_backend/controller/UsuarioController.java
+++ src/main/java/com/example/cashly_backend/controller/UsuarioController.java
@@ -1,10 +1,14 @@
 package com.example.cashly_backend.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import com.example.cashly_backend.entity.Usuario;
 import com.example.cashly_backend.repository.UsuarioRepository;
 
@@ -14,35 +18,41 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioRepository repository;
-     
+
     @GetMapping
     public List<Usuario> listarUsuarios() {
         return repository.findAll();
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> listarUsuario(@PathVariable Integer id) {
-        Optional<Usuario> usuario = repository.findById(id);
-        
-        if (usuario.isPresent()) {
-            return ResponseEntity.ok(usuario.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
+
     @PostMapping
     public Usuario cadastrarUsuario(@RequestBody Usuario usuario) {
         return repository.save(usuario);
     }
 
     @PutMapping("/{id}")
-    public Usuario editarUsuario(@PathVariable Integer id, @RequestBody Usuario usuario){
+    public Usuario editarUsuario(@PathVariable Integer id, @RequestBody Usuario usuario) {
         usuario.setId_usuario(id);
         return repository.save(usuario);
     }
+
     @DeleteMapping("/{id}")
     public String deletarUsuario(@PathVariable Integer id) {
         repository.deleteById(id);
         return "Usuario com ID: " + id + ", deletado!";
-        
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> credentials) {
+        return repository.findByEmail(credentials.get("email"))
+                .filter(u -> u.getSenha().equals(credentials.get("senha")))
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
 }
